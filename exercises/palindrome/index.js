65011fb2b6e6f8efc45f580f63fd2ce078c2adc8
Bruno Mertiku <bruno.mertiku@icitapeng.us>
2019-01-11 13:39:41 +0100

    step 1

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,15 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+
+    let reversed = '';
+
+    for(let char of str){
+        reversed = char + reversed;
+    }
+
+    return str == reversed;
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+
+//     const arr = str.split('');
+
+//     arr.reverse();
+
+//     return arr.join('');
+// }
+
+function reverse(str) {
+
+    let reversed = '';
+
+    for(let ch of str){
+        reversed = ch + reversed;
+    }
+
+    return reversed;
+}
+
+
+// function reverse(str) {
+//     debugger;
+//     return str.split('').reduce((reversed, character) =>  character + reversed, '')
+// }
+
 
 module.exports = reverse;
